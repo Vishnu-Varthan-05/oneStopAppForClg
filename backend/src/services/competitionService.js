015ffaccb5602df259e623e1a2efeb5a82aa6fd8
Vishnuvarthan M <131575256+Vishnu-Varthan-05@users.noreply.github.com>
2024-09-06 19:47:11 +0530

    Added getCompetition service

---
+++ backend/src/services/competitionService.js
@@ -0,0 +1,34 @@
+const { get_query_database } = require("../config/database_utils");
+
+exports.getCompetition = async(student, year, department, page=1, limit=20)=>{
+    try {
+        const offset = (page-1)* limit;
+        const query = `
+            SELECT
+                c.id,
+                c.name,
+                c.hostedby,
+                c.importancelvl,
+                CASE 
+                    WHEN cw.student IS NOT NULL THEN 1
+                    ELSE 0
+                END AS isWishlist
+            FROM competition c
+            LEFT JOIN competition_wishlist cw
+            ON c.id = cw.competition AND cw.student = ?
+            WHERE(
+                (c.year = ? AND c.department = ?) OR
+                (c.year = 5 AND c.department = ?) OR
+                (c.year = ? AND c.department = 24) OR
+                (c.year = 5 AND c.department = 24)
+            )
+            AND c.expiresAt > NOW()
+            LIMIT ? OFFSET ?
+        `;
+        const results = await get_query_database(query, [student, year, department, department, year, limit, offset]);
+        return results;
+    } catch (error) {
+        throw new Error(`Error retrieving Competitions: ${error.message}`);
+        
+    }
+}
